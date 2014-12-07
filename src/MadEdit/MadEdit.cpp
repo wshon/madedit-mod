adcedb2594180bec579e045e7b900cc2ba7584e4
liminggang <minggang.li@gmail.com>
2014-12-07 22:32:19 +0800

    Minor fix in print bookmark

--- src/MadEdit/MadEdit.cpp
+++ src/MadEdit/MadEdit.cpp
@@ -2320,34 +2320,15 @@ void MadEdit::PaintTextLines(wxDC *dc, const wxRect &rect, int toprow, int rowco
             {
                 int l=rect.GetLeft();
                 //static wxColor bm_BgColor = *wxLIGHT_GREY;
-                m_Syntax->SetAttributes(aeLineNumber);
                 if(m_DisplayLineNumber)
                 {
                     // paint bg
+                    m_Syntax->SetAttributes(aeLineNumber);
                     if(m_Syntax->nw_BgColor != bgcolor)
                     {
                         dc->SetPen(*wxThePenList->FindOrCreatePen(m_Syntax->nw_BgColor, 1, wxSOLID));
                         dc->SetBrush(*wxTheBrushList->FindOrCreateBrush(m_Syntax->nw_BgColor));
                         dc->DrawRectangle(l, row_top, m_LineNumberAreaWidth, m_RowHeight);
-                    }
-                    if(m_DisplayBookmark)
-                    {
-						/*if (bm_BgColor != bgcolor)
-                        {
-							dc->SetPen(*wxThePenList->FindOrCreatePen(bm_BgColor, 1, wxSOLID));
-							dc->SetBrush(*wxTheBrushList->FindOrCreateBrush(bm_BgColor));
-                            dc->DrawRectangle(l+m_LineNumberAreaWidth, row_top, m_BookmarkWidth+1, m_RowHeight);
-                        }*/
-
-                        // add: gogo, 27.09.2009
-                        if ( m_Lines->m_LineList.IsBookmarked(lineiter) )
-                        {
-                            dc->SetPen(*wxThePenList->FindOrCreatePen(bgcolor, 1, wxSOLID));
-							m_Syntax->SetAttributes(aeBookmark);
-							dc->SetBrush(*wxTheBrushList->FindOrCreateBrush(/*wxColour(0,0,192)*/m_Syntax->nw_Color));
-							dc->DrawCircle(l + m_LineNumberAreaWidth + m_BookmarkWidth / 2, row_top + m_RowHeight / 2,
-                                            m_RowHeight < 16 ? m_RowHeight/2 : 8 );
-                        }
                     }
                     if(displaylinenumber)
                     {
@@ -2371,24 +2352,45 @@ void MadEdit::PaintTextLines(wxDC *dc, const wxRect &rect, int toprow, int rowco
                             }
                         }
                     }
+                    
+                    if(m_DisplayBookmark)
+                    {
+                        /*if (bm_BgColor != bgcolor)
+                        {
+                            dc->SetPen(*wxThePenList->FindOrCreatePen(bm_BgColor, 1, wxSOLID));
+                            dc->SetBrush(*wxTheBrushList->FindOrCreateBrush(bm_BgColor));
+                            dc->DrawRectangle(l+m_LineNumberAreaWidth, row_top, m_BookmarkWidth+1, m_RowHeight);
+                        }*/
+
+                        // add: gogo, 27.09.2009
+                        if ( m_Lines->m_LineList.IsBookmarked(lineiter) )
+                        {
+                            l=rect.GetLeft();
+                            dc->SetPen(*wxThePenList->FindOrCreatePen(bgcolor, 1, wxSOLID));
+                            m_Syntax->SetAttributes(aeBookmark);
+                            dc->SetBrush(*wxTheBrushList->FindOrCreateBrush(/*wxColour(0,0,192)*/m_Syntax->nw_Color));
+                            dc->DrawCircle(l + m_LineNumberAreaWidth + m_BookmarkWidth / 2, row_top + m_RowHeight / 2,
+                                            m_RowHeight < 16 ? m_RowHeight/2 : 8 );
+                        }
+                    }
                 }
                 else
                 {
                     if(m_DisplayBookmark)
                     {
-						/*if (bm_BgColor != bgcolor)
+                        /*if (bm_BgColor != bgcolor)
                         {
-							dc->SetPen(*wxThePenList->FindOrCreatePen(bm_BgColor, 1, wxSOLID));
-							dc->SetBrush(*wxTheBrushList->FindOrCreateBrush(bm_BgColor));
+                            dc->SetPen(*wxThePenList->FindOrCreatePen(bm_BgColor, 1, wxSOLID));
+                            dc->SetBrush(*wxTheBrushList->FindOrCreateBrush(bm_BgColor));
                             dc->DrawRectangle(l, row_top, m_BookmarkWidth+1, m_RowHeight);
                         }*/
 
                         // add: gogo, 27.09.2009
                         if ( m_Lines->m_LineList.IsBookmarked(lineiter) )
                         {
-							dc->SetPen(*wxThePenList->FindOrCreatePen(bgcolor, 1, wxSOLID));
-							m_Syntax->SetAttributes(aeBookmark);
-							dc->SetBrush(*wxTheBrushList->FindOrCreateBrush(/*wxColour(0,0,192)*/m_Syntax->nw_Color));
+                            dc->SetPen(*wxThePenList->FindOrCreatePen(bgcolor, 1, wxSOLID));
+                            m_Syntax->SetAttributes(aeBookmark);
+                            dc->SetBrush(*wxTheBrushList->FindOrCreateBrush(/*wxColour(0,0,192)*/m_Syntax->nw_Color));
                             dc->DrawCircle( l + m_BookmarkWidth/2, row_top + m_RowHeight/2,
                                             m_RowHeight < 16 ? m_RowHeight/2 : 8 );
                         }
@@ -2436,11 +2438,11 @@ void MadEdit::PaintTextLines(wxDC *dc, const wxRect &rect, int toprow, int rowco
             int width = 80 * GetUCharWidth(0x20);
             if(*wxLIGHT_GREY!= bgcolor)
             {
-    		    dc->SetPen(*wxThePenList->FindOrCreatePen(*wxLIGHT_GREY, 1, wxSOLID));
+                dc->SetPen(*wxThePenList->FindOrCreatePen(*wxLIGHT_GREY, 1, wxSOLID));
             }
             else
             {
-				dc->SetPen(*wxThePenList->FindOrCreatePen(wxColour(128, 128, 128), 1, wxSOLID));
+                dc->SetPen(*wxThePenList->FindOrCreatePen(wxColour(128, 128, 128), 1, wxSOLID));
             }
             x1 += width+m_LeftMarginWidth;
             dc->DrawLine(x1, y, x1, y+(totalrow*m_RowHeight));
